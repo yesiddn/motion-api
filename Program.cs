2ba1d9dda40bebf9ff6fa84420aa97cde77f114e
Duvan Yesid <yesidrodriguez305@gmail.com>
2026-05-09 00:38:29 -0500

    Add services and endpoints for API key and node management

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using motion_api;
+using motion_api.Application.ApiKeys;
+using motion_api.Application.Nodes;
 using motion_api.Application.Reports;
 
 Env.Load();
@@ -16,9 +18,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddValidation();
 builder.Services.AddScoped<IReportService, ReportService>();
+builder.Services.AddScoped<INodeService, NodeService>();
+builder.Services.AddScoped<IApiKeyService, ApiKeyService>();
 
 var app = builder.Build();
 
 app.MapReportEndpoints();
+app.MapNodeEndpoints();
+app.MapApiKeyEndpoints();
 
 app.Run();
